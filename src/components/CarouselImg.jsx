84a4ad621b89b0f1f32dc9227fa42d178534d8b8
fabian1718 <fagodu5@gmail.com>
2022-10-13 13:59:52 -0500

    limpie puesto de trabajo

---
+++ src/components/CarouselImg.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CarouselImg = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CarouselImg;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './styles/Main.css'
+import 'bootswatch/dist/lux/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

284885474f3531ab69dfbc2d672943602d1021f0
fabian1718 <fagodu5@gmail.com>
2022-10-13 14:58:40 -0500

    footer y porject

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import '../styles/Footer.css'
 
 const Footer = () => {
+
     return (
-        <div>
+        <div className='footer'>
+            <p>copyright©Fabian FullStack</p>
+            <a 
+                className='linkedin' 
+                href='https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile'
+                target="_blank"
+            >
+                <i className="fa-brands fa-linkedin"></i>
+            </a>
             
         </div>
     );
